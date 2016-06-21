974f025469daff7dea6dfceb67ac14d6a4ed9d2a
nikhataly <nikhataly@gmail.com>
2016-06-21 17:09:05 -0400

    syntax

--- script.rb
+++ script.rb
@@ -2,4 +2,14 @@ require 'rubygems'
 require 'bundler/setup'
 require 'cucumber'
 require 'page-object'
-require 'selenium-webdriver
+require 'selenium-webdriver'
+
+
+# class HomePage
+#   include PageObject
+
+
+# end
+
+driver = Selenium::WebDriver.for :firefox
+driver.navigate_to "http://google.com"
