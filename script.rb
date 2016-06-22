bc42758c28549ff28a0ed2caa84ae13911c710f1
nikhataly <nikhataly@gmail.com>
2016-06-22 14:02:59 -0400

    Using page object methods

--- script.rb
+++ script.rb
@@ -5,11 +5,17 @@ require 'page-object'
 require 'selenium-webdriver'
 
 
-# class HomePage
-#   include PageObject
+class VisitHomePage
+  include PageObject
+  page_url "https://www.joefresh.com/ca/"
 
-
-# end
+  text_field(:first_time, :id => 'notification-modal')
+end
 
 browser = Selenium::WebDriver.for :firefox
-browser.get "https://www.joefresh.com/ca/"
+home_page = VisitHomePage.new(browser)
+home_page.goto
+home_page.first_time?
+
+
+## notification-modal
